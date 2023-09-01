113ef0bb68ec48d2229e5c22329657a712ee511c
Ripat-2345 <ripatalsafar@gmail.com>
2023-09-01 18:22:40 +0700

    feat: created edit profile

--- lib/ui/pages/profile_edit_page.dart
+++ lib/ui/pages/profile_edit_page.dart
@@ -1,3 +1,7 @@
+import 'package:bank_sha_app/shared/shared_method.dart';
+import 'package:bank_sha_app/shared/theme.dart';
+import 'package:bank_sha_app/ui/widgets/buttons.dart';
+import 'package:bank_sha_app/ui/widgets/forms.dart';
 import 'package:flutter/material.dart';
 
 class ProfileEditPage extends StatelessWidget {
@@ -5,6 +9,56 @@ class ProfileEditPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Edit Profile"),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        children: [
+          const SizedBox(height: 30),
+          Container(
+            padding: const EdgeInsets.all(22),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: whiteColor,
+            ),
+            child: Column(
+              children: [
+                // ! NOTE: User Name Input
+                const CustomFormField(
+                  title: "User Name",
+                ),
+                const SizedBox(height: 16),
+
+                // ! NOTE: Full Name Input
+                const CustomFormField(
+                  title: "Full Name",
+                ),
+                const SizedBox(height: 16),
+
+                // ! NOTE: Email Input
+                const CustomFormField(
+                  title: "Email Address",
+                ),
+                const SizedBox(height: 16),
+
+                // ! NOTE: Password Input
+                const CustomFormField(
+                  title: "Password",
+                  obscureText: true,
+                ),
+                const SizedBox(height: 30),
+
+                CustomFilledButton(
+                  title: "Continue",
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
